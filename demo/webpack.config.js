c8956b7ca9a53a2890652948c5ca66f5cc4e9d43
moujintao <moujintao@moujintaodeMacBook-Pro.local>
2017-07-03 15:42:28 +0800

    增加项目测试demo

---
+++ demo/webpack.config.js
@@ -0,0 +1,38 @@
+// var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const path = require('path');
+const FontIconCreatePlugin = require('../index.js');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js',
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js',
+        // libraryTarget: 'umd',
+        // library: ['Foo', '[name]']
+    },
+    module: {
+        rules: [
+            { test: /\.svg$/, loader: 'vusion-iconmaker', include: path.join(__dirname, 'src/icons') },
+            { test: /\.css$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+            { test: /\.(woff|eot|ttf|svg)$/, loader: 'url-loader', exclude: path.join(__dirname, 'src/icons') }
+        ]
+    },
+    devtool: '#eval-source-map',
+    plugins: [
+        new FontIconCreatePlugin({
+            name:"icon",
+            output:{
+                font:"/src/iconfont",
+                css:"/src/css/font",
+                html:"/src/iconfont"
+            },
+            suffix:{
+                css:".mcss",
+                html:".html"
+            },
+            //publishPath:"/publish/font/"
+        }),
+    ],
+}
